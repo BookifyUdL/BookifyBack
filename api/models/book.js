f06130282d2262c3a5e575ccde163b1a88848c2a
Korakk8 <oscarlopezluna@gmail.com>
2019-11-10 17:57:10 +0100

    Book model created

---
+++ api/models/book.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const BookSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  num_pages: {
+      type: Number,
+      required: true
+  },
+  publication_date: { 
+    type: Date,
+    required: true
+  },
+  genre: {
+      type: [Genre],
+      required: true
+  },
+  cover_image: {
+    type: String,
+    required: true
+  }
+});
+
+var BookModel = mongoose.model('User', BookSchema);
+
+module.exports = BookModel;
