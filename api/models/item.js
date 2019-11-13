822e6df4e169693b7c118dd183e110224c01eff0
Korakk <oscarlopezluna@gmail.com>
2019-11-13 18:31:38 +0100

    Added item and shop models

---
+++ api/models/item.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  shop_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Shop',
+      required: true
+  },
+  book_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true
+  },
+  price: {
+    type: String,
+    required: true,
+    unique: true
+  }
+
+});
+
+var ItemModel = mongoose.model('Item', ItemSchema);
+
+module.exports = ItemModel;
