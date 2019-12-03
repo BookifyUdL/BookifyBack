2824c41fb86b59efe09e5a6a45a7806aa52b7227
rspaimoc2 <raduspaimoc@gmail.com>
2019-12-03 20:54:32 +0100

    Scrapped book from fnac

--- script/com_bestseller.py
+++ script/com_bestseller.py
@@ -9,6 +9,11 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import json
 #import pandas as pd
+genres_list = []
+casa_del_libro_id = 0
+corte_ingles_id = 0
+fnac_id = 0
+
 def check_param_name(received, extracted):
     words = received.split(" ")
     for word in words:
@@ -23,6 +28,26 @@ def scrap_book_from_fnac(title, author):
         aux = title
     url = "https://www.fnac.es/SearchResult/ResultList.aspx?SCat=0%211&Search=" + aux + " " + author + "&sft=1&sa=0"
     print url
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    basicwrap = soup.find_all(class_='articleList Article-list')
+    for product in basicwrap:
+        bookInfo = (product.find(class_='clearfix Article-item js-Search-hashLinkId'))
+        userPriceInfo = product.find(class_='userPrice')
+        bookUrl = userPriceInfo['href']
+        bookTitle = userPriceInfo['title']
+        bookPrice = userPriceInfo.get_text().strip()
+
+        bookInfo = (product.find(class_='Article-infoContent'))
+        bookInfo = (product.find(class_='Article-descSub'))
+        bookAuthor =  bookInfo.find('a').get_text()
+
+        bookCheck = (aux.upper() in bookTitle.upper()) or (check_param_name(aux, bookTitle))
+        authorCheck = (author.upper() in bookAuthor.upper()) or (check_param_name(author, bookAuthor))
+        if(bookCheck or authorCheck):
+            return bookUrl, price
+
+    return None, None
 
 def scrap_book_from_corte_ingles(title, author):
     if("(" in title):
@@ -50,19 +75,113 @@ def scrap_book_from_corte_ingles(title, author):
                 return bookUrl, price
     return None, None
 
+def add_genre_by_name(genre):
+    resultGenres = db.genres.find_one({'name': genre['name']})
+    authorId = 0
+    if resultGenres == None:
+        insertResult = db.genres.insert_one(genre)
+        resultGenres = db.genres.find_one({'name': genre['name']})        
+        genreId = resultGenres['_id']
+    else:
+        authorId = resultGenres['_id']
+    genres_list.append(authorId)
+
+def add_genres(db):
+    db.genres.delete_many({})
+    genre1 = {}
+    genre1['name'] = 'Biography'
+    genre1['picture'] = 'genre1'
+    add_genre_by_name(genre1)
+
+    genre2 = {}
+    genre2['name'] = 'Computing / Interenet'
+    genre2['picture'] = 'genre2'
+    add_genre_by_name(genre2)
+
+    genre3 = {}
+    genre3['name'] = 'Crime'
+    genre3['picture'] = 'genre3'
+    add_genre_by_name(genre3)
+
+    genre4 = {}
+    genre4['name'] = 'Education'
+    genre4['picture'] = 'genre4'
+    add_genre_by_name(genre4)
+
+    genre5 = {}
+    genre5['name'] = 'Literature'
+    genre5['picture'] = 'genre5'
+    add_genre_by_name(genre5)
+
+    genre6 = {}
+    genre6['name'] = 'Health'
+    genre6['picture'] = 'genre6'
+    add_genre_by_name(genre6)
+
+    genre7 = {}
+    genre7['name'] = 'Romance'
+    genre7['picture'] = 'genre7'
+    add_genre_by_name(genre7)
+
+    genre8 = {}
+    genre8['name'] = 'Fantasy'
+    genre8['picture'] = 'genre8'
+    add_genre_by_name(genre8)
+
+    genre9 = {}
+    genre9['name'] = 'Adventure'
+    genre9['picture'] = 'genre9'
+    add_genre_by_name(genre9)
+
+def add_shops(db):
+    db.shops.delete_many({})
+    shop1 = {}
+    shop1['name'] = 'Casa del Libro'
+    shop1['url'] = 'https://www.cuponeto.com/wp-content/uploads/2019/01/casa-del-libro-descuentos.png'
+    resultGenres = db.shops.find_one({'name': shop1['name']})
+    if resultAuthors == None:
+        insertResult = db.shops.insert_one(shop1)
+        resultGenres = db.shops.find_one({'name': shop1['name']})
+        casa_del_libro_id = resultGenres['_id']
+    else:
+        casa_del_libro_id = resultGenres['_id']
+
+    shop2 = {}
+    shop2['name'] = 'El Corte Ingles'
+    shop2['url'] = 'https://www.elcorteingles.es/recursos/informacioncorporativa/img/portal/2017/07/06/el-corte-ingles-triangulo.png'
+    resultGenres = db.shops.find_one({'name': shop2['name']})
+    if resultAuthors == None:
+        insertResult = db.shops.insert_one(shop1)
+        resultGenres = db.shops.find_one({'name': shop2['name']})
+        corte_ingles_id = resultGenres['_id']
+    else:
+        corte_ingles_id = resultGenres['_id']
+
+    shop3 = {}
+    shop3['name'] = 'Fnac'
+    shop3['url'] = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Fnac_Logo.svg/499px-Fnac_Logo.svg.png'
+    resultGenres = db.shops.find_one({'name': shop3['name']})
+    if resultAuthors == None:
+        insertResult = db.shops.insert_one(shop3)
+        resultGenres = db.shops.find_one({'name': shop1['name']})
+        fnac_id = resultGenres['_id']
+    else:
+        fnac_id = resultGenres['_id']
 
 page_no = 1
 file = open('com_book.csv', 'w')
-#writer = csv.writer(file)
-#data = ['Title', 'URL-Cover', 'Author', 'Description', 'Extension', 'Language', 'Isbn', 'Year', 'Price']
-#writer.writerow(data)
 
-#client = MongoClient("mongodb+srv://admin:U2D8PSgwPKhNdJoX@cluster0-nxv9z.mongodb.net/test?retryWrites=true&w=majority")
-#db = client.test
-#db.books.delete_many({})
+client = MongoClient("mongodb+srv://admin:U2D8PSgwPKhNdJoX@cluster0-nxv9z.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
+db.books.delete_many({})
+db.items.delete_many({})
+db.shops.delete_many({})
+
+add_genres(db)
+add_genres(db)
 
 while page_no < 3:
-
+    genres_index = 0
     try:
         if page_no == 1:
             string = ''
@@ -141,22 +260,23 @@ while page_no < 3:
 
 
                     #print data
-                    genreObject = {}
-                    genreObject['name'] = 'Literature'
-                    genreObject['picture'] = 'https://webstockreview.net/images/clipart-book-symbol-17.png'
+                    #genreObject = {}
+                    #genreObject['name'] = 'Literature'
+                    #genreObject['picture'] = 'https://webstockreview.net/images/clipart-book-symbol-17.png'
+
 
                     authorObject = {}
                     authorObject['name'] = author
 
 
-                    # resultAuthors = db.authors.find_one({'name': author})
-                    # authorId = 0
-                    # if resultAuthors == None:
-                    #     insertResult = db.authors.insert_one(authorObject)
-                    #     resultAuthors = db.authors.find_one({'name': author})
-                    #     authorId = resultAuthors['_id']
-                    # else:
-                    #     authorId = resultAuthors['_id']
+                    resultAuthors = db.authors.find_one({'name': author})
+                    authorId = 0
+                    if resultAuthors == None:
+                        insertResult = db.authors.insert_one(authorObject)
+                        resultAuthors = db.authors.find_one({'name': author})
+                        authorId = resultAuthors['_id']
+                    else:
+                        authorId = resultAuthors['_id']
 
                     toinsert = {}
                     toinsert['title'] = title
@@ -166,18 +286,46 @@ while page_no < 3:
                     #toinsert['author'] = authorId
                     toinsert['genre'] = {}
                     toinsert['cover_image'] = picture
-                    toinsert['comments'] = {}
-                    toinsert['genre'] = genreObject
-                    toinsert['price'] = price
-                    # _v_ = db.books.insert_one(toinsert)
-                    # print _v_
+                    toinsert['comments'] = []
+                    #toinsert['genre'] = genreObject
+                    toinsert['genre'] = genres_list[genres_index % (len(genres_list) - 1)]
+                    genres_index += 1
+                    #toinsert['price'] = price
+                    _v_ = db.books.insert_one(toinsert)
+                    resultVerga = db.books.find_one({'title': title})
+                    bookId = resultVerga['_id']
+
+                    #inset item as casa del libro
+                    cdl = {}
+                    cdl['shop_id'] = casa_del_libro_id
+                    cdl['book_id'] = bookId
+                    cdl['price'] = float(price)
+                    cdl['url'] = currentBookUrl
+                    db.items.insert_one(cdl)
+
+
                     print "Scrapping book from corte ingles"
                     corteInglesBookURl, corteInglesBookPrice = scrap_book_from_corte_ingles(title, author)
                     if corteInglesBookURl != None and corteInglesBookPrice != None:
-                        print "Extraido"
-                        print corteInglesBookURl
-                        print corteInglesBookPrice
-                    #exit(0)
+                        cdl = {}
+                        cdl['shop_id'] = corte_ingles_id
+                        cdl['book_id'] = bookId
+                        cdl['price'] = float(corteInglesBookPrice)
+                        cdl['url'] = corteInglesBookURl
+                        db.items.insert_one(cdl)
+
+                    print "Scrapping book from fnac"
+                    fnacBookURl, fnacBookPrice = scrap_book_from_fnac(title, author)
+                    if fnacBookURl != None and fnacBookURl != None:
+                        cdl = {}
+                        cdl['shop_id'] = fnac_id
+                        cdl['book_id'] = bookId
+                        cdl['price'] = float(fnacBookPrice)
+                        cdl['url'] = fnacBookURl
+                        db.items.insert_one(cdl)
+
+
+                    print "Todo correcto"
                 except Exception as e:
                     pageNum = None
                     language = None
